7d121776d7e64d92768545b047c87c34bb5660f8
Virginia Wu <79432798+VirginiaWu11@users.noreply.github.com>
2021-09-26 14:13:28 -0400

    complete first test

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -1,13 +1,13 @@
 process.env.NODE_ENV = "test";
 
-const require = require("supertest");
+const request = require("supertest");
 const app = require("../app");
 const db = require("../db");
 
 let testCompany;
 beforeEach(async () => {
     const result = await db.query(
-        `INSERT INTO companies (code, name, description) VALUES ('google', 'Google') RETURNING  code, name, description`
+        `INSERT INTO companies (code, name, description) VALUES ('google', 'Google', 'search engine') RETURNING  code, name, description`
     );
     testCompany = result.rows[0];
 });
@@ -18,4 +18,12 @@ afterEach(async () => {
 
 afterAll(async () => {
     await db.end(); // sever the connection with the database
+});
+
+describe("GET /companies", () => {
+    test("Get a list with one company", async () => {
+        const res = await request(app).get("/companies");
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({ companies: [testCompany] });
+    });
 });
