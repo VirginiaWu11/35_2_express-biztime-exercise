8dc95a48e8888173e8ec92192a2051cdfd01fdc0
Virginia Wu <79432798+VirginiaWu11@users.noreply.github.com>
2021-09-26 21:03:11 -0400

    add createData function

--- _test-common.js
+++ _test-common.js
@@ -4,7 +4,16 @@ const db = require("./db");
 
 class testHelpers {
     static async createData() {
-        await db.query("");
+        await db.query(`INSERT INTO companies (code, name, description)
+        VALUES ('apple', 'Apple', 'Maker of OSX.'), 
+        ('ibm', 'IBM', 'Big blue.')`);
+        await db.query(
+            `INSERT INTO invoices (comp_code, amt, paid, add_date, paid_date)
+            VALUES ('apple', 100, false, '2018-01-01', null),
+                    ('apple', 200, true, '2018-02-01', '2018-02-02'), 
+                    ('ibm', 300, false, '2018-03-01', null)
+            RETURNING id`
+        );
     }
 
     static async deleteData() {
